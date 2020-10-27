8e2a5d02414d9c26bac4f7f4b29e9404db644ff5
FranekB <camil.bodziony@gmail.com>
2020-10-27 17:13:45 +0100

    Added skeleton of site and js modules

--- index.js
+++ index.js
@@ -1,14 +1,55 @@
 const gameBoard = (() => {
-  let gameboard = [["X", "X", "X"], ["O", "O", "X"], ["X", "O", "X"]];
-  return { gameboard };
+  let gameboard = [["X", "X", "X"], ["O", "O", "O"], ["X", "X", "X"]];
+
+  const getGameboard = () => gameboard;
+  return {
+    gameboard,
+    getGameboard
+  };
 })()
 
-const displayController = (() => {
-  let state = "waiting"
-  return { state }
-})
 
-const playerFactory = (name) => {
-  let playerName = name || "Anonymous player";
-  return { playerName };
+
+const playerFactory = (name, mark) => {
+  const getName = () => name;
+  const getMark = () => mark;
+
+  return { getName, getMark };
 }
+
+
+
+
+const displayControler = (() => {
+  let player1 = playerFactory("Player1", "X");
+  let player2 = playerFactory("Player2", "O");
+
+
+  const init = () => {
+    cacheDOM();
+    render();
+  }
+  const cacheDOM = () => {
+    gameboardContainer = document.querySelector(".gameboard")
+    return gameboardContainer
+  }
+
+  const createSquareElement = (value) => {
+    let element = document.createElement("div")
+    element.classList.add("gameboard-square")
+    element.textContent = value
+    return element
+  }
+
+  const render = () => {
+    for (rows of gameBoard.getGameboard()){
+      for (cols of rows){
+        gameboardContainer.appendChild(createSquareElement(cols));
+      }
+    }
+  }
+
+  init()
+
+
+})()
